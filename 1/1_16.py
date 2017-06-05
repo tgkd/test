c05ab167cfa1979ee71b3036c759e8af268e305c
Trofimov Pavel <pavtrof342@gmail.com>
2017-06-05 10:18:27 +0500

    fix

--- 1/1_16.py
+++ 1/1_16.py
@@ -1,11 +1,16 @@
-def cross(a, b):
-    c = [a[1]*b[2] - a[2]*b[1],
-         a[2]*b[0] - a[0]*b[2],
-         a[0]*b[1] - a[1]*b[0]]
+def inner_prod(v1, v2):
+    """inner production of two vectors."""
+    summ = 0
+    for i in range(v1):
+        summ += v1[i] * v2[i]
+    return summ
 
-    return c
+
+def matmult3(m, v):
+    return [inner_prod(r, v) for r in m]
+
 
 av = [1, 2, 3]
 bv = [4, 5, 6]
 
-print(cross(av, bv))
+print(matmult3(av, bv))
